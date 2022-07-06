711a3aad905ce95e2494423e83311b5c2cf1074d
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2022-07-06 19:18:36 +0500

    add rider data

--- includes/header.php
+++ includes/header.php
@@ -2,16 +2,17 @@
 <div class="header">
     <div class="container">
         <div class="header-left">
-            <span class="menu"><img src="images/menu.png" alt=""/></span>
+            <span class="menu"><img src="/images/menu.png" alt=""/></span>
                 <ul class="nav1">
                     <li><a href="index.php">HOME</a></li>
-                    <li><a class="active" href="about.php">ABOUT</a></li>
+                    <li><a class="active" href="/about.php">ABOUT</a></li>
                     <li><a href="food-available.php"> AVAILABLE FOOD LIST</a></li>
                     
                     <li><a href="contact.php">CONTACT</a></li>
                     <li><a href="donor/login.php">DONOR</a></li>
+                    <li><a href="rider/login.php">RIDER</a></li>
                     <li><a href="admin/login.php">ADMIN</a></li>
-                    
+
                 </ul>
                 <!-- script for menu -->
                     <script> 
@@ -24,7 +25,7 @@
                 <!-- //script for menu -->
         </div>
         <div class="header-right">
-            <a href="index.php"><img src="images/logo.png" alt=""></a>
+            <a href="/index.php"><img src="/images/logo.png" alt=""></a>
         </div>
         <div class="clearfix"></div>
     </div>
